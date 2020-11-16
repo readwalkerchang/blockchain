5db4158e25566726c9a6a07f56d4a2930aaf8129
readwalkerchang <readwalkerchang@gmail.com>
2020-11-16 18:01:09 +0100

    add createNewBlock method, and then  add newBlock object inside the method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -2,3 +2,10 @@ function Blockchain(){
     this.chain = [];
     this.newTransations = [];
 }
+
+Blockchain.prototype.createNewBlock 
+= function(nouce, previousBlockHash,hash){
+    const newBlock = {
+        index : this.chain.length + 1,
+    }; 
+}

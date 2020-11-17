651e4c2d15189097c47e9e05fa6014f395fc858c
readwalkerchang <readwalkerchang@gmail.com>
2020-11-17 15:50:08 +0100

    testing creating  mutiple blocks

--- dev/test.js
+++ dev/test.js
@@ -1,4 +1,9 @@
 const Blockchain = require('./blockchain') //Include Modules
 
-const bitconin = new Blockchain;
-console.log(bitconin);
+const bitcoin = new Blockchain;
+bitcoin.createNewBlock(Math.random(),'hfiuahuif','hfuised849hf');
+bitcoin.createNewBlock(Math.random(),'hfiuahuif','hfuised849hf');
+bitcoin.createNewBlock(Math.random(),'hfiuahuif','hfuised849hf');
+
+
+console.log(bitcoin);

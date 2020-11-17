e60dc5ab31c8a27ca1a50b56f9d6b3d5955872bc
readwalkerchang <readwalkerchang@gmail.com>
2020-11-17 15:13:07 +0100

    finish createNewBlock method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -7,5 +7,13 @@ Blockchain.prototype.createNewBlock
 = function(nouce, previousBlockHash,hash){
     const newBlock = {
         index : this.chain.length + 1,
-    }; 
+        timestamp : Date.now(),
+        Transactions : this.newTransations,
+        nouce : nouce, 
+        hash: hash,
+        previousBlockHash: previousBlockHash,
+    };
+    this.newTransations = [];
+    this.chain.push(newBlock);
+    return newBlock;
 }

65f7e28ef212434dfb8a66753b0f89a2cc6349c9
readwalkerchang <readwalkerchang@gmail.com>
2020-11-17 15:29:07 +0100

    testing blockchain constructor function with Node.js Modules

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -17,3 +17,5 @@ Blockchain.prototype.createNewBlock
     this.chain.push(newBlock);
     return newBlock;
 }
+
+module.exports = Blockchain; //export modules
---
+++ dev/test.js
@@ -0,0 +1,4 @@
+const Blockchain = require('./blockchain') //Include Modules
+
+const bitconin = new Blockchain;
+console.log(bitconin);

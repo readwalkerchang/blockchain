41ef05bf00d856d1d82329faca3e2f2a5b321db2
readwalkerchang <readwalkerchang@gmail.com>
2020-11-23 08:42:20 +0100

    Test pending transactions

--- dev/test.js
+++ dev/test.js
@@ -3,4 +3,9 @@ const Blockchain = require('./blockchain') //Include Modules
 const bitcoin = new Blockchain;
 bitcoin.createNewBlock(Math.random(),'hfiuahuif','hfuised849hf');
 bitcoin.createNewTransaction(100,'Joe','Kay');
+bitcoin.createNewBlock(Math.random(),'hfiuahuif','hfuised849hf');
+bitcoin.createNewTransaction(200,'Joe','Kay');
+bitcoin.createNewTransaction(300,'Joe','Kay');
+bitcoin.createNewTransaction(400,'Joe','Kay');
+
 console.log(bitcoin);

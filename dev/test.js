cc6718fa1ec4a198fb491572545cda2d539b8969
readwalkerchang <readwalkerchang@gmail.com>
2020-12-08 20:24:43 +0100

    test: genesis block

--- dev/test.js
+++ dev/test.js
@@ -1,24 +1,6 @@
 const Blockchain = require('./blockchain'); //Include Modules
 
-const bitcoin = new Blockchain;
+const bitcoin = new Blockchain();
 
-const previousBlockHash = '0000HKNBJFHFBJMHQ';
-const currentBlockData = [
-    {
-        amount: 10,
-        sender: 'HDWU144532IKJH98472',
-        recipient: '14552F84JF8SJHK',
-    },
-    {
-        amount: 100,
-        sender: '1238FNJKFSJKL3',
-        recipient: '12345FJHK38TRBIJ254',
-    },
-    {
-        amount: 30,
-        sender: '132453248BNF87FB',
-        recipient: '954FEWHJKUHE2434587FU2',
-    },
-];
-let nonce = 123698;
-console.log(bitcoin.hashBlock(previousBlockHash,currentBlockData,nonce));
+
+console.log(bitcoin);

9dc5d7d1cfcabc821164182f6f66cbe7e8f5d7b2
Dusing Chang <dusing@MacBook-Air-van-Dusing.local>
2020-12-24 09:56:29 +0100

    Building API endpoints

--- dev/api.js
+++ dev/api.js
@@ -1,8 +1,18 @@
 let express = require('express');
 let app = express();
 
-app.get('/',function(req,res){
-    res.send('Hello World');
+app.get('/blockchain',function(req,res){
+    
 });
 
-app.listen(4000);
+app.get('/blockchain',function(req,res){
+    
+});
+
+app.get('/blockchain',function(req,res){
+    
+});
+
+app.listen(4000, function(){
+    console.log('listening on port 4000');
+});

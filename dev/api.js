48c5076837f34ce1ffe0f333224ffbae336417ca
Dusing Chang <dusing@MacBook-Air-van-Dusing.local>
2021-01-08 17:26:19 +0100

    test: postman with express routing

--- dev/api.js
+++ dev/api.js
@@ -1,5 +1,5 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
 
 app.get('/blockchain',function(req,res){
     
@@ -9,10 +9,12 @@ app.get('/blockchain',function(req,res){
     
 });
 
-app.get('/blockchain',function(req,res){
-    
+app.post('/transaction',function(req,res){
+    console.log(req.body);
+    res.send(`the amount of transaction is:${req.body} bitcoin.`);
 });
 
-app.listen(4000, function(){
-    console.log('listening on port 4000');
+app.listen(3000, function(){
+    console.log('listening on port 3000...');
 });
+
